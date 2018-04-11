b725655a5b306444f214ae5690f3662319b5d9a1
Kyle <kshifflett707@gmail.com>
2018-04-10 21:05:08 -0700

    Configured project to render react

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Code Fight Club</h1>
+        <br/>
+        <br/>
+        <h3>We have one rule... don't talk about code fight club</h3>
+      </div>
+    )
+  }
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http')
+var socketIO = require('socket.io')
+
+const PORT = 3000;
 
 var app = express();
 
-console.log('sup merge conflict test #3');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/../client/dist'));
 
-var test = function(val) {
-    return val;
-}
+var server = http.createServer(app);
+
+
+server.listen(PORT, () => console.log(`listening on port:${PORT}`));
