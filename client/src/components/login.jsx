eeeb31c01d875ce199c2ab7e7afa73bb36b87ec0
brandon-villiados <brandon.villiados@gmail.com>
2018-04-12 16:18:15 -0700

    Post from login successful

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import $ from 'jquery';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        showModalLogin: false
+        showModalLogin: false,
+        username: '',
+        password: ''
     };
     this.openModalLogin = this.openModalLogin.bind(this);
     this.submitLogin = this.submitLogin.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
   }
 
   openModalLogin () {
@@ -17,6 +22,18 @@ class Login extends Component {
 
   submitLogin () {
     this.setState({ showModalLogin: false });
+    $.post('/login', this.state)
+    .done((data) => {
+      console.log('this login was... ', data);
+    });
+  }
+
+  onChangeUsername (e) {
+    this.setState({ username: e.target.value });
+  }
+
+  onChangePassword (e) {
+    this.setState({ password: e.target.value });
   }
 
   render () {
@@ -31,9 +48,9 @@ class Login extends Component {
           <h1>Login</h1>
           <form>
             Username:
-            <br/><input type="text" name="username"/><br/>
+            <br/><input type="text" name="username" onChange={this.onChangeUsername}/><br/>
             Password:
-            <br/><input type="text" name="password"/><br/>
+            <br/><input type="text" name="password" onChange={this.onChangePassword}/><br/>
           </form>
         </div>
           <button onClick={this.submitLogin}>Submit</button>
