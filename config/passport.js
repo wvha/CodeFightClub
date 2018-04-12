a5f449976f71e18429425a7823a48fe681df7930
Kyle <kshifflett707@gmail.com>
2018-04-12 15:29:20 -0700

    cleaned up server side authentication, slight change to database in order to function as expected

--- config/passport.js
+++ config/passport.js
@@ -1,41 +1,64 @@
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../database/index.js').User;
 
-module.exports = function(passport) {
+module.exports = (passport) => {
+
+  console.log('within passport: ');
+
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
 
   passport.deserializeUser(function(id, done) {
     //TODO: create this function in User model
-    User.getUserById(id, function(err, user) {
+    User.findById(id, function(err, user) {
       done(err, user);
     });
   });
 
-  passport.use('local-login', new LocalStrategy(
-    function(username, password, done) {
-      //TODO: Create this function in User model
-      User.getUserByUsername(username, function(err, user) {
-        if (err) {
-          throw err;
-        }
-        if (!user) {
-          return done(null, false, {message: 'Unknown user'});
-        }
-        //TODO: Create this function in User model
-        User.comparePassword(password, user.password, function(err, isMatch) {
-          if (err) {
-            throw err;
-          }
-          if (isMatch) {
-            return done(null, user);
-          } else {
-            return done(null, false, {message: "invalid Password"});
-          }
+  passport.use('local-signup', new LocalStrategy(
+    {
+      username: 'username',
+      password: 'password',
+      passReqToCallback: true,
+    },
+    function(req, username, password, callback) {
+      console.log('within local-signup of passport:  ');
+      process.nextTick(function() {
+        User.findOne({'username': username}, function(err, user) {
+          if (err) return callback(err);
+          if (user) return callback(null, false, 'username taken');
+
+          var newUser = new User();
+          newUser.username = username;
+          newUser.password = password;
+          newUser.save(function(err) {
+            if (err) console.log('error in newUser.save ' + err);
+            console.log(newUser);
+            return callback(null, newUser, 'successful signup');
+          });
         });
       });
     }
   ));
+
+  passport.use('local-login', new LocalStrategy(
+    {
+      username: 'username',
+      password: 'password',
+      passReqToCallback: true,
+    },
+    function(req, username, password, callback) {
+      console.log('Inside passport local strat for login');
+      process.nextTick(function() {
+        User.findOne({'username': username}, function(err, user) {
+          if (err) return callback(err);
+          if (!user) return callback(null, false, 'user not found');
+          if (user.password !== password) return callback(null, false, 'incorrect password');
+          return callback(null, user, 'success login');
+        })
+      })
+    }
+  ))
 
 }
--- database/index.js
+++ database/index.js
@@ -9,31 +9,46 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', () => {
-  
-  const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    wins: Number,
-    losses: Number,
-    score: Number
-  });
-  
-  const ToyProblemSchema = new mongoose.Schema({
-    title: String,
-    prompt: String,
-    functionName: String,
-    tests: [TestSchema]
-  });
-  
-  // * Create helper functions in 'helpers' import file and assign to methods
-
-  const TestSchema = new mongoose.Schema({
-    // Todo: Needs to be updated when we know test format
-  })
-
-const ToyProblem = mongoose.Model('ToyProblem', ToyProblemSchema);
-
-const User = mongoose.Model('User', UserSchema);
-
+  console.log('Connected to db...');
 });
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  wins: Number,
+  losses: Number,
+  score: Number
+});
+
+// const ToyProblemSchema = new mongoose.Schema({
+//   title: String,
+//   prompt: String,
+//   functionName: String,
+//   tests: [TestSchema]
+// });
+
+// * Create helper functions in 'helpers' import file and assign to methods
+
+// const TestSchema = new mongoose.Schema({
+//   // Todo: Needs to be updated when we know test format
+// })
+
+// const ToyProblem = mongoose.Model('ToyProblem', ToyProblemSchema);
+
+const User = mongoose.model('User', UserSchema);
+
+//User collection functions
+const getUserById = function(id, callback) {
+  return User.findById(id, callback);
+};
+
+
+// Database export
+module.exports.db = db;
+
+//User collection export
+module.exports.User = User;
+
+//User functions
+module.exports.getUserById = getUserById;
