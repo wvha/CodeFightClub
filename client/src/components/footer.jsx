6a0a1ba634645f1cb6d7c3b547a1f36bf80151f3
tbhesswebber <tbhesswebber@gmail.com>
2018-04-17 05:19:15 -0700

    Primary front-end functionality styled

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
 
-class Footer extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {};
-  }
-
-  render () {
-    return (
-      <div className="container row" id="footer">
-      </div>
-    );
-  }
+const Footer = function(props) {
+  return (
+    <div className="container row" id="footer">
+      <span>&#169; 2018</span>
+    </div>
+  );
 }
 
 export default Footer;
