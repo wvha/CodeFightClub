6c5cf82434697b9bc1e1a0f855fb3f15aff7b2a1
Kyle <kshifflett707@gmail.com>
2018-04-18 19:35:46 -0700

    Done refactoring post to /solution

--- helpers/sandbox.js
+++ helpers/sandbox.js
@@ -1,5 +1,6 @@
 const Sandbox = require('sandbox');
 const box = new Sandbox();
+const Promise = require('bluebird');
 
 var execute = function(code) {
   return new Promise((resolve) => {
@@ -10,26 +11,4 @@ var execute = function(code) {
   });
 };
 
-// var tests = [ {input: '5, 6', expected: '11'}, {input: '3, 4', expected: '8'} ];
-// var solution = 'function add(a, b) { return a + b; }';
-// var funcName = 'add';
-// var status;
-// var testRes = [];
-
-// tests.forEach((test) => {
-//   execute(`${solution} ${funcName}(${test.input})`)
-//   .then((data) => {
-//     console.log(data);
-//     if (data !== test.expected) {
-//       status = 'fail';
-//     } else {
-//       status = 'pass';
-//     }
-//     testRes.push({input: test.input, expected: test.expected, actual: data, status: status});
-//     console.log(testRes);
-//   });
-// });
-
 module.exports.execute = execute;
-
-// [ { input: '5, 6', actual: '30', expected: '11'}, { input: '3, 4', actual: '12', expected: '7'} ]
