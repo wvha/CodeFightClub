cbb911d2ffe309099a28365ce58981bf4d24f984
wvha <Wvha17@gmail.com>
2018-04-25 17:33:40 -0700

    edited add toy problem page for clarity

--- client/src/components/addToyProblem.jsx
+++ client/src/components/addToyProblem.jsx
@@ -41,7 +41,7 @@ class AddToyProblems extends React.Component {
     }
 
     someTests() {
-        return `Tests || Example [{"input": "2,3" , "expected": "5"}, {"input": "4, 4", "expected": "8"}]"`;
+        return `Tests || Example [{"input": "2,3" , "expected": "5"}, {"input": "4, 4", "expected": "8"}]`;
     }
 
     render () {
@@ -52,22 +52,26 @@ class AddToyProblems extends React.Component {
                     <h5>Challenge Title</h5>
                     <input className="halfw" placeholder="Problem name" value={this.state.title} onChange={this.setter.call(this, 'title')}></input>
                 </div>
+                <br />
                 <div className="fullw">
                     <h5>Function Name</h5>
                     <input className="halfw" placeholder="Expected function name" value={this.state.code} onChange={this.setter.call(this, 'code')}></input>
                 </div>
+                <br />
                 <div className="fullw">
                     <h5>Parameters || Example: value1, value2</h5>
                     <input className="halfw" placeholder="Expected function parameters as comma separated values" value={this.state.params} onChange={this.setter.call(this, 'params')}></input>
                 </div>
+                <br />
                 <div id="newPrompt" className="fullw">
                     <h5>Prompt</h5>
                     <textarea placeholder="Instructions for user" value={this.state.body} onChange={this.setter.call(this, 'body')} className="tp-text fullw"></textarea>
                 </div>
+                <br />
                 <div id="newTests" className="fullw">
                     <h5>{this.someTests()}</h5>
                     <textarea
-                    placeholder='Create tests as [{"input": "2,3" , "expected": "5"}, {"input": "4, 4", "expected": "8"}]'
+                    placeholder='Create tests as an array of objects with "input" property and "expected" property, in JSON-parsable format'
                     onChange={this.setter.call(this, 'tests')}
                     value={this.state.tests}
                     className="tp-text fullw"
