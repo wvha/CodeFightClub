9c57165f620b655d8fc50c952ca890982248abc5
Christina <christina.elaine.yuen@gmail.com>
2018-04-27 10:42:33 -0700

    Changed variable name

--- client/src/components/timer.jsx
+++ client/src/components/timer.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 const Timer = (props) => {
   let timer;
-  if (props.timer > 0 && props.view === 'waitingRoom') {
+  if (props.timerTillNextGame > 0 && props.view === 'waitingRoom') {
     timer = (
       <span className="container">
         <span>Next Fight In: </span>
-        <button>{props.timer}</button>
+        <button>{props.timerTillNextGame}</button>
       </span>
     );
-  } else if (props.timer > 0) {
+  } else if (props.timerTillNextGame > 0) {
     timer = (
       <span className="container">
         <span>Next Fight In: </span>
-        <button>{props.timer}</button>
+        <button>{props.timerTillNextGame}</button>
         <button onClick={props.changeView('waitingRoom')}>Join Fight</button>
       </span>
     );
